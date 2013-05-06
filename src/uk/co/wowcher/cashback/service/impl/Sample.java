9dfbbc070085746758ebcb8ffabd6e44f2385675
abidahmed <md.abid123@gmail.com>
2013-05-06 17:20:35 +0530

    Sample files

---
+++ src/uk/co/wowcher/cashback/service/impl/Sample.java
@@ -0,0 +1,13 @@
+package uk.co.wowcher.cashback.service.impl;
+
+public class Sample {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
